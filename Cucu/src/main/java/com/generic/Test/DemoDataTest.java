6b650dc3108b5f031cc0c4ca5972d40c41d543a0
QSPJSP <QSPJSP@DESKTOP-I59386F>
2022-08-04 13:34:52 +0530

    MyFirstPush

---
+++ Cucu/src/main/java/com/generic/Test/DemoDataTest.java
@@ -0,0 +1,12 @@
+package com.generic.Test;
+
+import org.testng.annotations.Test;
+
+public class DemoDataTest {
+	@Test
+	public void getData()
+	{
+		System.out.println("M1 Running");
+	}
+    
+}
